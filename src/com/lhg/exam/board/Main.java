6798c362bca26733b05a3dc0a5a32d3cd820808c
LHG313 <dd31391@gmail.com>
2021-12-01 22:59:59 +0900

    /usr/article/detail 명령으로 최근 게시물 보기

--- src/com/lhg/exam/board/Main.java
+++ src/com/lhg/exam/board/Main.java
@@ -2,47 +2,67 @@ package com.lhg.exam.board;
 
 import java.util.Scanner;
 
+/*
+    ### TODO 예시
+    - [.] /usr/article/detail 입력처리
+    - [ ] /usr/article/detail 입력되면 가장 최근 게시물 정보 노출
+    */
+
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-      System.out.println("== 게시판 v 0.1 ==");
-      System.out.println("== 프로그램 시작 ==");
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
 
-      int articlesLastId = 0;
+    int articlesLastId = 0;
+    Article lastArticle = null;
 
-      while (true) {
-        System.out.printf("명령 ) ");
-        String cmd = sc.nextLine();
+    while (true) {
+      System.out.printf("명령 ) ");
+      String cmd = sc.nextLine();
 
-        if (cmd.equals("exit")) {
-          break;
-        }
-
-       else if (cmd.equals("/usr/article/write")) {
-          System.out.println("-게시물 등록-");
-          System.out.printf("제목 : ");
-          String title = sc.nextLine();
-          System.out.printf("내용 : ");
-          String body = sc.nextLine();
-
-          int id = articlesLastId + 1;
-          articlesLastId = id;
-
-          Article article = new Article(id, title, body);
-          System.out.println("생성된 게시물 객체 : " + article);
-          System.out.printf("%d번 게시물이 입력되었습니다.\n",article.id);
-        }
-
-       else {
-          System.out.printf("입력된 명령어 : %s\n", cmd);
-        }
+      if (cmd.equals("exit")) {
+        break;
       }
-      System.out.println("== 프로그램 종료 ==");
+      else if (cmd.equals("/usr/article/detail")) {
+        System.out.println("-게시물 상세내용-");
 
-        sc.close();
+        if (lastArticle == null){
+          System.out.println("게시물이 등록되지 않았습니다.");
+          continue;
+        }
+        Article article = lastArticle;
 
+        System.out.printf("번호 : %d\n",article.id);
+        System.out.printf("제목 : %s\n",article.title);
+        System.out.printf("내용 : %s\n",article.body);
+      }
+      else if (cmd.equals("/usr/article/write")) {
+        System.out.println("-게시물 등록-");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+
+        int id = articlesLastId + 1;
+        articlesLastId = id;
+
+        Article article = new Article(id, title, body);
+        lastArticle = article;
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 입력되었습니다.\n",article.id);
+      }
+
+      else {
+        System.out.printf("입력된 명령어 : %s\n", cmd);
+      }
     }
+    System.out.println("== 프로그램 종료 ==");
+
+    sc.close();
+
+  }
 }
 
 class Article{
@@ -50,14 +70,14 @@ class Article{
   String title;
   String body;
 
-Article(int id , String title, String body){
-  this.id = id;
-  this.title = title;
-  this.body = body;
-}
+  Article(int id , String title, String body){
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
 
-@Override
+  @Override
   public String toString(){
-  return String.format("{id : %d , title : \"%s\", body : \"%s\"}",id, title,body);
-}
+    return String.format("{id : %d , title : \"%s\", body : \"%s\"}",id, title,body);
+  }
 }
